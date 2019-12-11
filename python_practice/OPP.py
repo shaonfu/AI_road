23722644826212d823195031efea559604fcd057
shaonfu <qq3011@163.com>
2019-12-11 23:00:31 +0800

    company_commit

--- python_practice/OPP.py
+++ python_practice/OPP.py
@@ -329,3 +329,78 @@ if __name__ =='__main__':
     main()
 
 # %%
+#面向对象
+"""
+三大支柱:封装、继承、多态
+__init__是一个特殊方法用于在创建对象时进行初始化操作
+月薪结算系统 - 部门经理每月15000,程序员每小时200,销售员1800底薪加销售额5%提成
+"""
+from abc import ABCMeta,abstractmethod
+
+class Employee(metaclass=ABCMeta):
+    """员工(抽象类)"""
+    def __init__(self,name):
+        self.name = name
+    
+    @abstractmethod
+    def get_salary(self):
+        """结算月薪(抽象方法)"""
+        pass
+
+class Manager(Employee):
+    """部门经理"""
+
+    def get_salary(self):
+        return 15000.0
+
+class Programmer(Employee):
+    """部门经理"""
+
+    def __init__(self, name ,working_hour=0):
+        self.working_hour = working_hour
+        super().__init__(name)
+
+    def get_salary(self):
+        return 200.0 * self.working_hour
+
+class Salesman(Employee):
+    """销售员"""
+
+    def __init__(self,name,sales=0.0):
+        self.sales = sales
+        super().__init__(name)
+
+
+    def get_salary(self):
+        return 1800.0 + self.sales * 0.05
+
+class EmployeeFactory():
+    """创建员工的工厂(工厂模式 - 通过工厂实现对象使用者和对象之间的解耦合)"""
+    
+    @staticmethod
+    def create(emp_type,*args,**kwargs):
+        """创建员工"""
+        emp_type = emp_type.upper()
+        emp = None
+        if emp_type == 'M':
+            emp = Manager(*args,**kwargs)
+        elif emp_type == 'P':
+            emp = Programmer(*args,**kwargs)
+        elif emp_type == 'S':
+            emp = Salesman(*args,**kwargs)
+        return emp
+    
+def main():
+    """主函数"""
+    emps = [
+        EmployeeFactory.create('M','曹操'),
+        EmployeeFactory.create("P", "楚旸",120),
+        EmployeeFactory.create("P", "柔柔",85),
+        EmployeeFactory.create("S", "菲娴",125000),
+    ]
+
+    for emp in emps:
+        print('%s: %.2f元' % (emp.name,emp.get_salary()))
+    
+if __name__ == '__main__':
+    main()
