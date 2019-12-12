bb7dc33efc91e44caa6d907678492b8bdc1c9b47
shaonfu <qq3011@163.com>
2019-12-12 23:47:34 +0800

    resnet_commit

--- tensorflow_practice/tensorflow_2.x_Tutorials/07-Inception/main.py
+++ tensorflow_practice/tensorflow_2.x_Tutorials/07-Inception/main.py
@@ -28,7 +28,9 @@ class ConvBNRelu(keras.Model):
         super(ConvBNRelu,self).__init__()
 
         self.model = keras.models.Sequential([
-
+            keras.layers.Conv2D(ch,kernel_size,strides = strides, padding = padding),
+            keras.layers.BatchNormalization(),
+            keras.layers.RELU()
         ])
 
     def call(self,x,training=None):
@@ -44,14 +46,64 @@ class InceptionBlk(keras.Model):
         super(InceptionBlk,self).__init__()
 
         self.ch = ch
+        self.strides = strides
+
+        self.conv1 = ConvBNRelu(ch, strides = strides)
+        self.conv2 = ConvBNRelu(ch, kernel_size=3,strides = strides)
+        self.conv1 = ConvBNRelu(ch, kernel_size=3,strides = strides)
+        self.conv1 = ConvBNRelu(ch, kernel_size=3,strides = 1)
+
+        self.pool = keras.layers.MaxPooling2D(3,strides=1,padding = 'same')
+        self.pool_conv = ConvBNRelu(ch, strides = strides)
     
     def call(self,x,training=None):
+
+        x1 = self.conv1(x,training = training)
+
+        x2 = self.conv2(x,training = training)
+
+        x3_1 = self.conv3_1(x,training = training)
+        x3_2 = self.conv3_2(x3_1,training = training)
+
+        x4 = self.pool(x)
+        x4 = self.pool_conv(x4,training = training)
+
+        #concat along axis = channel
+        x = tf.concat([x1,x2,x3_2,x4],axis = 3)
 
         return x
 
 class Inception(keras.Model):
     def __init__(self,num_layers,num_classes,init_ch=16,**kwargs):
         super(Inception,self).__init__(**kwargs)
+
+        self.in_channels = init_ch
+        self.out_channels = init_ch
+        self.num_layers = num_layers
+        self.init = init_ch
+
+        self.conv1 = ConvBNRelu(init_ch)
+
+        self.block = keras.models.Sequential(name = 'dynamic-blocks')
+
+        for block in range(num_layers):
+
+            for layer_id in range(2):
+
+                if layer_id ==0:
+                    block = InceptionBlk(self.out_channels,strides = 2)
+                
+                else:
+                    block = InceptionBlk(self.out_channels, strides=1)
+
+                self.blocks.add(block)
+            
+            #enlarger out_channels per block
+            self.out_channels *= 2
+
+        self.avg_pool = keras.layers.GlobalAveragePooling2D()
+        self.fc = keras.layers.Dense(num_classes)
+
 
     def call(self,x,training = None):
         out = self.conv1(x,training = training)
